d7eee0f79593ce3e1aff16046f3375315e948eaf
Vinicius Craveiro - Peon <viniciuscraveiroo@gmail.com>
2022-10-12 13:50:08 -0300

    Aula 2.5.1

--- src/main/java/br/com/vinicraveiro/vendas/controller/VendedorController.java
+++ src/main/java/br/com/vinicraveiro/vendas/controller/VendedorController.java
@@ -1,10 +1,15 @@
 package br.com.vinicraveiro.vendas.controller;
 
+import br.com.vinicraveiro.vendas.VendasApplication;
 import br.com.vinicraveiro.vendas.domain.Vendedor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/vendedor")
@@ -12,7 +17,19 @@ public class VendedorController {
     //GET | POST | PUT | DELETE
 
     @GetMapping
-    public ResponseEntity<String> nomeVendedor(){
-        return ResponseEntity.ok("Vinicius");
+    public ResponseEntity<List<Vendedor>> listar(){
+        Vendedor um = new Vendedor();
+        um.setId(1L);
+        um.setNome("Joao");
+
+        Vendedor dois = new Vendedor();
+        dois.setId(2L);
+        dois.setNome("Mauricio");
+
+        var vendedores = Arrays.asList(um, dois);
+
+        return ResponseEntity.ok(vendedores);
     }
+
+
 }
